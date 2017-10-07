a9a747f6ec43a8d92fa884eaad4af2b20730b8a1
Rafał Lorenz <vardius@gmail.com>
2017-10-07 16:31:41 +1100

    Update appengine.go

--- appengine.go
+++ appengine.go
@@ -3,32 +3,43 @@
 package golog
 
 import (
-	"golang.org/x/net/context"
+	"context"
+
 	"google.golang.org/appengine/log"
 )
 
 type (
-	appengineLogger struct{}
+	appengineLogger struct {
+		verboseLevel int
+	}
 )
 
-func NewAppengineLogger() Logger {
-	return &appengineLogger{}
+func NewAppengineLogger(verbose string) Logger {
+	return &appengineLogger{parseVerboseLevel(verbose)}
 }
 
 func (logger *appengineLogger) Debug(ctx context.Context, format string, args ...interface{}) {
-	log.Debugf(ctx, format, args...)
+	if logger.verboseLevel > DebugLevel {
+		log.Debugf(ctx, format, args...)
+	}
 }
 
 func (logger *appengineLogger) Info(ctx context.Context, format string, args ...interface{}) {
-	log.Infof(ctx, format, args...)
+	if logger.verboseLevel > InfoLevel {
+		log.Infof(ctx, format, args...)
+	}
 }
 
 func (logger *appengineLogger) Warning(ctx context.Context, format string, args ...interface{}) {
-	log.Warningf(ctx, format, args...)
+	if logger.verboseLevel > WarningLevel {
+		log.Warningf(ctx, format, args...)
+	}
 }
 
 func (logger *appengineLogger) Error(ctx context.Context, format string, args ...interface{}) {
-	log.Errorf(ctx, format, args...)
+	if logger.verboseLevel > ErrorLevel {
+		log.Errorf(ctx, format, args...)
+	}
 }
 
 func (logger *appengineLogger) Critical(ctx context.Context, format string, args ...interface{}) {
