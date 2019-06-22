467fc17bc5666c7d6041e0b9bc3eaa1174bf06ef
Rafał Lorenz <vardius@gmail.com>
2019-06-22 12:39:27 +1000

    Fix const iota values

--- example_test.go
+++ example_test.go
@@ -23,6 +23,24 @@ func ExampleNewConsoleLogger() {
 	// [31;1mERROR: Error 666
 }
 
+func ExampleNewConsoleLogger_second() {
+	ctx := context.Background()
+
+	logger := golog.NewConsoleLogger(golog.Verbose(2))
+
+	logger.SetFlags(0)
+
+	// logger.Info won't print to the console because it is below loglevel "warn"
+	logger.Info(ctx, "%s", "Info")
+	// logger.Warning and log.Error will both get printed
+	logger.Warning(ctx, "%s %d", "Warn", 1)
+	logger.Error(ctx, "%s %d", "Error", 666)
+
+	// Output:
+	// [33;1mWARN: Warn 1
+	// [31;1mERROR: Error 666
+}
+
 func ExampleDebug() {
 	ctx := context.Background()
 	logger := golog.New(golog.Debug)
